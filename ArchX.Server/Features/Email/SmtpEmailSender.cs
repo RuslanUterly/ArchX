7a6e59f72e22d28a50c918ad7800eddc3f40a4f1
Ruslan_Uterly <kholikov.rus@mail.ru>
2026-05-27 21:58:00 +0300

    добавил логирование

--- ArchX.Server/Features/Email/SmtpEmailSender.cs
+++ ArchX.Server/Features/Email/SmtpEmailSender.cs
@@ -1,15 +1,26 @@
 using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace ArchX.Server.Features.Email;
 
-public class SmtpEmailSender(IOptions<SmtpOptions> options) : IEmailSender
+public class SmtpEmailSender(IOptions<SmtpOptions> options, ILogger<SmtpEmailSender> logger) : IEmailSender
 {
     private readonly SmtpOptions options = options.Value;
 
     public async Task SendAsync(string toEmail, string subject, string htmlBody)
     {
+        logger.LogInformation(
+            "SMTP options resolved. Host={Host}, Port={Port}, EnableSsl={EnableSsl}, UserName={UserName}, FromEmail={FromEmail}, FromName={FromName}, PasswordSet={PasswordSet}",
+            options.Host,
+            options.Port,
+            options.EnableSsl,
+            options.UserName,
+            options.FromEmail,
+            options.FromName,
+            !string.IsNullOrWhiteSpace(options.Password));
+
         if (string.IsNullOrWhiteSpace(options.Host))
             throw new InvalidOperationException("SmtpOptions:Host не задан в конфигурации.");
 
@@ -36,5 +47,6 @@ public class SmtpEmailSender(IOptions<SmtpOptions> options) : IEmailSender
         }
 
         await client.SendMailAsync(message);
+        logger.LogInformation("Password reset email sent to {ToEmail}", toEmail);
     }
 }
